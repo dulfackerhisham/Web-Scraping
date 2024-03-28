d6fbdc77633a076c7b4d99667af9a297b2d6fd6f
Hisham Dulfacker <dulfackerhisham@gmail.com>
2024-03-28 18:14:15 +0530

    program done

--- 02 - Requests & Data filterations/main.py
+++ 02 - Requests & Data filterations/main.py
@@ -12,22 +12,39 @@
 
 from bs4 import BeautifulSoup
 import requests
+import time
 
-# step 1 ->  get the url of the website
-url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation="
-page = requests.get(url)
+print('Put some Skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
 
-# step 2 -> creating beautiful soup instance and finding setting up tag
-soup = BeautifulSoup(page.content, 'lxml')
-# step 2.1 -> finding the tag which all the jobs are under
-jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
-#step 2.2 -> finding company name & skills with the jobs tag
-for job in jobs:
-    published_date = job.find('span', class_ = 'sim-posted').span.text
-    if 'few' in published_date:
-        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
-        skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
-        print(f'''
-        Company Name: {company_name}
-        Required Skills: {skills}
-        ''')
+def find_jobs():
+    # step 1 ->  get the url of the website
+    url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation="
+    page = requests.get(url)
+
+    # step 2 -> creating beautiful soup instance and finding setting up tag
+    soup = BeautifulSoup(page.content, 'lxml')
+    # step 2.1 -> finding the tag which all the jobs are under
+    jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+    #step 2.2 -> finding company name & skills with the jobs tag
+    for job in jobs:
+        published_date = job.find('span', class_ = 'sim-posted').span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_ = 'srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
+
+            if unfamiliar_skill not in skills:
+                print(f'Company_name: {company_name.strip()}')
+                print(f'Required Skills: {skills.strip()}')
+                print(f'More info: {more_info}')
+                print('')
+
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} minutes...')
+        time.sleep(time_wait * 60)
